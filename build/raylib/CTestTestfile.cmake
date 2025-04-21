082f46653666a45d7e5554c0948ea10ac4502f86
Dan Adrian Mirea <damirea84@gmail.com>
2025-04-21 09:31:26 +0300

    remove build folder

--- build/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/raylib/raylib
-# Build directory: D:/adi/src/raylib/raypong/build/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
