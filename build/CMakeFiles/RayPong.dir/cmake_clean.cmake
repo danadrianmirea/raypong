e3eb42ad1e58402fab91ea638a208ee3912cb7dc
Dan Adrian Mirea <damirea84@gmail.com>
2025-04-21 09:18:50 +0300

    add cmake support, reorganise source files

---
+++ build/CMakeFiles/RayPong.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RayPong.dir/src/ball.cpp.obj"
+  "CMakeFiles/RayPong.dir/src/ball.cpp.obj.d"
+  "CMakeFiles/RayPong.dir/src/cpupaddle.cpp.obj"
+  "CMakeFiles/RayPong.dir/src/cpupaddle.cpp.obj.d"
+  "CMakeFiles/RayPong.dir/src/game.cpp.obj"
+  "CMakeFiles/RayPong.dir/src/game.cpp.obj.d"
+  "CMakeFiles/RayPong.dir/src/globals.cpp.obj"
+  "CMakeFiles/RayPong.dir/src/globals.cpp.obj.d"
+  "CMakeFiles/RayPong.dir/src/main.cpp.obj"
+  "CMakeFiles/RayPong.dir/src/main.cpp.obj.d"
+  "CMakeFiles/RayPong.dir/src/paddle.cpp.obj"
+  "CMakeFiles/RayPong.dir/src/paddle.cpp.obj.d"
+  "RayPong.exe"
+  "RayPong.exe.manifest"
+  "RayPong.pdb"
+  "libRayPong.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RayPong.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/RayPong.dir/src/globals.cpp.obj.d
@@ -0,0 +1,10 @@
+CMakeFiles/RayPong.dir/src/globals.cpp.obj: \
+ D:\adi\src\raylib\raypong\src\globals.cpp C:/raylib/raylib/src/raylib.h \
+ D:/msys64/mingw64/lib/gcc/x86_64-w64-mingw32/14.2.0/include/stdarg.h \
+ D:/msys64/mingw64/include/stdarg.h \
+ D:/msys64/mingw64/include/_mingw_stdarg.h \
+ D:/msys64/mingw64/include/vadefs.h D:/msys64/mingw64/include/_mingw.h \
+ D:/msys64/mingw64/include/_mingw_mac.h \
+ D:/msys64/mingw64/include/_mingw_secapi.h \
+ D:/msys64/mingw64/include/sdks/_mingw_ddk.h \
+ D:\adi\src\raylib\raypong\src\globals.h
